e92cb8c88386af2062972355eb8c5c80b1af1df5
Koós Balázs Bence <koosbalazsbence@gmail.com>
2024-10-14 13:40:31 +0200

    create nextjs app

--- halloween/src/app/page.tsx
+++ halloween/src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-3xl sm:text-5xl font-bold text-center sm:text-left">
-          🕷️ Halloween
+        <h1 className="text-4xl font-bold text-center sm:text-left">
+          Halloween
         </h1>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
