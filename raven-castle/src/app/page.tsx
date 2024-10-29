82c03fd4755a1ba1b162419be99441457e7c16aa
Koós Balázs Bence <koosbalazsbence@gmail.com>
2024-10-29 22:30:27 +0100

    adding the discovery articles separately

--- raven-castle/src/app/page.tsx
+++ raven-castle/src/app/page.tsx
@@ -1,46 +1,20 @@
 // components
-import HeroImage from "@/components/HeroImage";
+import HeroBackgournd from "@/components/HeroBackground";
 import Sponsors from "@/components/Sponsors";
 import Quote from "@/components/Quote";
+import BookingArticle from "@/components/BookingArticle";
 
 export default function Home() {
   return (
     <>
-      <div className="relative w-full min-h-screen flex flex-col items-center justify-center">
-        <HeroImage />
-        <div className="font-serif absolute top-1/2 z-20 flex flex-col items-center gap-2 text-5xl text-white transform -translate-y-1/2">
-          <h1>Élmény.</h1>
-          <h1>Kaland.</h1>
-          <h1>Felfedezés.</h1>
-          <button className="castleIntroBtn font-inter bg-[#3E6027] px-20 py-1 mt-6 rounded-sm uppercase text-lg tracking-widest hover:bg-[#284714] transition duration-200 ease-in-out">
-            A Vár Bemutatása
-          </button>
-        </div>
-      </div>
+      <HeroBackgournd />
       <div>
         <Sponsors />
       </div>
       <div>
         <Quote />
       </div>
-      <div>
-        <h1 className="text-center text-3xl m-12">Fedezze fel a várat és foglaljon időpontot még ma!</h1>
-      </div>
-      <div className="grid grid-cols-3 gap-4 items-center justify-items-center">
-        <div className="grid grid-rows-2">
-          <h1 className="text-2xl">Nyitvatartás</h1>
-          {/* TODO: Insert table HERE */}
-        </div>
-        <div className="grid grid-rows-2">
-          <h1 className="text-2xl">Megközelítés</h1>
-          {/* TODO: Insert text HERE */}
-        </div>
-        <div className="grid grid-rows-2">
-          <h1 className="text-2xl">Időpontfoglalás</h1>
-          <p className="text-sm font-light">Egyeztessen kollégáinkkal telefonon!</p>
-          {/* TODO: Insert calendar HERE */}
-        </div>
-      </div>
+      <BookingArticle />
     </>
-  );
+  )
 }
