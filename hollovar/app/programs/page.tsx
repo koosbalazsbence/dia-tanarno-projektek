b854ce5dfc87f54042cf7e4920cb290887bb7bbc
Koós Balázs Bence <koosbalazsbence@gmail.com>
2024-11-10 20:14:56 +0100

    add hero component

---
+++ hollovar/app/programs/page.tsx
@@ -0,0 +1,9 @@
+import { Hero } from "../components/Hero"
+
+export default function Programs() {
+  return (
+    <div className="w-8/12 mx-auto">
+      <Hero title="Események" />
+    </div>
+  )
+}
